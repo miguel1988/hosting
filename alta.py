193ae8603f6ff20c286747467663a7dd0b2a83eb
miguel1988 <migue306@hotmail.com>
2014-02-13 10:51:58 +0100

    terminando el alta, ha que revisar su funcionamiento

--- alta.py
+++ alta.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*- 
 
+
 # Importamos las librerías necesarias:
 import os
 import sys
 import MySQLdb
 import string
+
 
 # Parametros variables de conexión base datos:
 maquina = 'localhost'
@@ -12,12 +14,15 @@ usuario = 'root'
 clave = 'root'
 based_datos = 'ftpd'
 
+
 # Recepción de argumentos (nombre usuario y dominio):
 nombre_usuario =(sys.argv[1])
 nombre_dominio =(sys.argv[2])
 
+
 # Conexión a con la base de datos de MySQL:
 conexion_bd = MySQLdb.connect(host=maquina, user=usuario, passwd=clave, db=base_datos)
+
 
 # Consulta existencia de usuario:
 cursor = conexion_bd.cursor()
@@ -30,6 +35,7 @@ if resp_username != None:
 else:
 	print "El usuario introducido no esta registrado puede regisrarse como %s" %nombre_usuario
 	
+	
 # Consulta existencia de dominio:
 select_dominio = "select dominio from usuarios where dominio='%s';" %nombre_dominio
 cursor.execute(select_dominio)
@@ -40,6 +46,21 @@ if resp_dominio != None:
 else:
 	print "El dominio introducido no esta registrado puede registrarlo como %s" %nombre_dominio
 
+# Obtenemos el UID mas alto registrado en la base de datos:
+select_uid = "select uid from usuarios order by uid desc limit 1;"
+cursor.execute(select_uid)
+uid_obtenido = cursor.fetchall()
+uid_nuevo = uid_obtenido[0][0]+1
+uid_nuevo = str(uid_nuevo)
+
+
+# Creamos el directorio del nuevo usuario:
+os.system("mkdir /srv/www/%s" %nombre_dominio)
+os.system("cp index.html /srv/www/%s" %nombre_dominion/index.html)
+os.system("chown "+uid_nuevo+":6000 /srv/www/"+nombre_dominio+"")
+os.system("chmod 755 /srv/www/"+nombre_dominio+"")
+
+
 # Generamos una contraseña de 8 digitos para el nuevo usuario:
 from random import choice
 def GenPasswd(n):
@@ -47,17 +68,35 @@ def GenPasswd(n):
 contrasenia_bd = GenPasswd(8)
 print "%s tu contraseña es %s" % (nombre_usuario, contrasenia_bd)
 
-# Creamos la base de datos y le otorgamos los privilegios al nuevo usuario:
+
+# Creamos la base de datos, le otorgamos los privilegios al nuevo usuario y lo registramos:
 crea_db = "create database %s" %nombre_usuario
 cursor.execute(crea_db)
 otorgar_privilegios = "grant all privileges on %s.* to "% (nombre_usuario)+ " %s@localhost"% (nombre_usuario)+ " identified by "+"'%s'" % (contrasenia_bd)+";"
 cursor.execute(otorgar_privilegios)
 recargar_bd = "flush privileges;"
+cxlinsert_usuario= "insert into `usuarios` values ('', "+"'"+nombre_usuario+"', password("+"'"+contrasenia_bd+"'), "+"'"+uid_nuevo+"', 6000, '/srv/www/"+nombre_dominio+"', '/sbin/nologin', 0, '', '');"
+cursor.execute(insert_usuario)
 conexion_bd.commit()
 
-# Creamos el directorio del nuevo usuario:
-os.system("mkdir /srv/www/%s" %nombre_usuario)
-os.system("cp index.html /srv/www/%s" %nombre_usuario/index.html)
+
+#  DNS
+
+# Añadimos al fichero /etc/bind/named.conf.local la nueva zona:
+zona_nueva = '\nzone ' +'"' +  nombre_dominio +'"'  +'{\ntype master;\nfile "db.'+ nombre_dominio +'"' +';\n}; '
+nameconflocal = open("/etc/bind/named.conf.local","a")
+nameconflocal.write(zona_nueva) 
+nameconflocal.close() 
+
+# Creamos el fichero en /var/cache/bind/ de la nueva zona:
+p_zona = open("p_zona","r")
+lineas = p_zona.readlines() 
+plantillazona.close()
+nueva_zona = open("/var/cache/bind/db."+nombre_dominio+"","w")
+for linea in lineas:
+	linea = linea.replace('nuevo_dominio',nombre_dominio)
+	ficherozona.write(linea)
+nueva_zona.close()
 
 
 # Editamos el fichero del virtualhost para el nuevo usuario:
@@ -71,7 +110,6 @@ vh=open("/etc/apache2/sites-available/%s" % nombre_dominio,"w")
 vh.write(texto_vh)
 vh.close()
 
-
 pma=open("p_myphpadmin","r")
 texto_pma=pma.read()
 pma.close()
@@ -83,3 +121,17 @@ pma.close()
 os.system("a2ensite phpmyadmin%s >/dev/null" % nombre_dominio)
 os.system("a2ensite %s >/dev/null" % nombre_dominio)
 os.system("service apache2 restart >/dev/null ")
+
+
+# Ultimo
+
+#Creamos el nuevo usuario en la base de datos
+print "Creando nuevo usuario..."
+insert_usuario= "insert into `usuarios` values ('', "+"'"+nombre_usuario+"', password("+"'"+contrasenia_bd+"'), "+"'"+uid_nuevo+"', 6000, '/srv/www/"+nombre_dominio+"', '/sbin/nologin', 0, '', '');"
+cursor.execute(insert_usuario)
+crear_usuario = "GRANT ALL ON "+user_name+".* TO "+user_name+"@localhost IDENTIFIED BY "+"'"+pass_user+"';"
+cursor.execute(crear_usuario)
+db.commit()
+db.close()
+
+
